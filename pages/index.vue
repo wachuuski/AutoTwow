146576e84d20c3664b269d4f5b3825f11d917c4b
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-05-12 00:10:22 -0700

    removed tutorial files

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <Tutorial/>
+  <h1>AutoTWOW yay! 🎉</h1>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
